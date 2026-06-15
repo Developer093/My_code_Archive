7e81dd84c4f1b13ceba6efcafacafcc2ae07163b
Kimsangwon <akg9380@gmail.com>
2026-06-16 01:31:11 +0900

    99_game_project_add_deal(2026.06.15)

--- python_project/99_game/deck.py
+++ python_project/99_game/deck.py
@@ -15,3 +15,17 @@ class Deck:
 
     def draw_card(self):
         return self.cards.pop()
+
+    def deal_cards(self, player_count, card_count):
+        hands = []
+
+        # 플레이어 수만큼 빈 손패 생성
+        for _ in range(player_count):
+            hands.append([])
+
+        # 카드 분배
+        for _ in range(card_count):
+            for hand in hands:
+                hand.append(self.draw_card())
+
+        return hands
--- python_project/99_game/main.py
+++ python_project/99_game/main.py
@@ -1,6 +1,7 @@
 
 # 구십구 99 프로젝트
-# Day 1 - 카드 덱 생성
+# Day 1 - 카드 덱 생성 + 카드 섞기 + 패 분배
+
 import random
 
 deck = []
@@ -29,12 +30,32 @@ for _ in range(3):
 # 특수 카드 J (1장)
 deck.append("J")
 
+# 카드 섞기
 random.shuffle(deck)
+
+# 플레이어와 컴퓨터의 패
+player_hand = []
+computer_hand = []
+
+# 5장씩 분배
+for _ in range(5):
+    player_hand.append(deck.pop())
+    computer_hand.append(deck.pop())
+
 # 결과 출력
-print("카드 덱")
+print("플레이어 패")
+print(player_hand)
+
+print()
+
+print("컴퓨터 패")
+print(computer_hand)
+
+print()
+
+print("남은 카드 덱")
 print(deck)
 
 print()
 
-print("총 카드 수 :", len(deck))
-
+print("남은 카드 수 :", len(deck))
