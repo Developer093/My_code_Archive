030f3c45c1c9cdd55b52d5010d13ad91bd2043cf
Kimsangwon <akg9380@gmail.com>
2026-06-18 01:38:44 +0900

    built_in_object_string

---
+++ vanillascript/built_in_object/object.js
@@ -0,0 +1,24 @@
+/**
+ * object객체 >> 모든 객체의 부모가 되는 객체
+ */
+let obj = {
+    name: "김상원",
+    gender: "male",
+    age:34
+}
+
+let obj_2 = {
+    name: "이준엽",
+    gender: "male",
+    age:30
+}
+
+let sword = {
+    list_name: "sword",
+    ATK: 10,
+    price: 150 
+}
+console.log(obj); // 김상원
+console.log(obj_2) // 이준엽
+console.log(sword) // sword
+
---
+++ vanillascript/built_in_object/string.js
@@ -0,0 +1,41 @@
+/**
+ * sting 객체는 문자열을 나타내는 빌트인 객체입니다.
+ * 
+ * 사용법 >> 변수명.메서드()
+ *          문자열.메서드()
+ */
+    // 1. length : 문자열의 길이를 반환
+    // How to use >> 문자열.length
+
+    const str_1 = "Hello, World!";
+    console.log(str_1.length); 
+
+    // 2. toUpperCase() : 문자열을 대문자로 변환
+    // How to use >> 문자열.toUpperCase()
+
+    const str_2 = "kimsangwon";
+    console.log(str_2.toUpperCase());
+
+    // 3. toLowerCase() : 문자열을 소문자로 변환
+    // How to use >> 문자열.toLowerCase()
+
+    const str_3 = "PENPINEAPPLE,APPLEPEN";
+    console.log(str_3.toLowerCase());
+
+    // 4. include() : 문자열이 특정 문자열을 포함하는지 여부를 반환
+    // How to use >> 문자열.include("찾을 문자열")
+
+    const str_4 = "javascript is intersting";
+    console.log(str_4.includes("java"));
+
+    // 5. slice() : 문자열의 일부를 추출하여 새로운 문자열을 반환
+    // How to use >> 문자열.slice(시작 인덱스, 끝 인덱스)
+
+    const text = "javascript is funny";
+    console.log(text.slice(0, 4));
+
+    // 6. replace() : 문자열의 일부를 다른 문자열로 대체
+    // How to use >> 문자열.replace("바꿀 문자열", "새로운 문자열")
+
+    const text_2 = "python is easy";
+    console.log(text_2.replace('python', 'javascript'));
